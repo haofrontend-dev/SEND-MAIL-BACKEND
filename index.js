0fbcd6463c7a358cdf0a2b77e04eb147e0ed69c2
quochao <hao.nguyen@widosoft.com>
2024-03-20 16:56:39 +0700

    fix router

--- index.js
+++ index.js
@@ -1,8 +1,40 @@
 const express = require("express");
+const morgan = require("morgan");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+const { errorHandler } = require("./src/common/helpers/errorHandle");
+
 const app = express();
+dotenv.config();
 
-app.get("/", (req, res) => res.send("Express on Vercel"));
+app.use(cors());
+app.use(express.json());
+app.use(
+    express.urlencoded({
+        extended: true,
+    })
+);
+app.get('/', (req, res) => {
+    return res.status(200).json({
+        ok: true,
+        message: 'Hello world'
+    })
+})
 
-app.listen(3000, () => console.log("Server ready on port 3000."));
+app.use("/api/v1", require("./src/app/v1/routes"));
+
+app.use((error, __, next) => {
+    next(error);
+});
+
+app.use((error, req, res, ____) => {
+    const resultError = errorHandler(error);
+    return res.status(resultError?.response.status).json(resultError?.response);
+});
+
+app.listen(5000, () => {
+    console.info(`💸 Api backend start with http://localhost:${5000} 🔥`);
+});
 
 module.exports = app;
